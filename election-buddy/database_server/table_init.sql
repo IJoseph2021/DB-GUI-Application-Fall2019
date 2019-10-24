88cf24eef026ff009e2319603be626fac14c042a
sshoemaker <sshoemaker@smu.edu>
2019-10-23 19:37:58 -0500

    Table_init for sprint 1

--- election-buddy/database_server/table_init.sql
+++ election-buddy/database_server/table_init.sql
@@ -1,4 +1,4 @@
-USE ELECTIONBUDDY;
+USE electionBuddy;
 CREATE TABLE USER (
 	ID INT(8) PRIMARY KEY,
     username varchar(15) UNIQUE,
@@ -6,4 +6,29 @@ CREATE TABLE USER (
     lname varchar(20),
     passhash varchar(100),
     email varchar(30));
+CREATE TABLE CANDIDATE (
+	userID INT(8),
+    partyCode INT(8),
+    zipCode varchar(10),
+    state varchar(2),
+	city varchar(25)
+);
+
+CREATE TABLE ADMIN(
+	userID INT(8),
+    adminLevel INT(8)
+);
+
+CREATE TABLE VOTER(
+	userID INT(8),
+    partyCode INT(8),
+    zipCode varchar(10),
+    state varchar(2),
+	city varchar(25)
+);
+
+CREATE TABLE PARTY(
+	partyCode INT(8) PRIMARY KEY,
+    partyName varchar(25)
+);
     
