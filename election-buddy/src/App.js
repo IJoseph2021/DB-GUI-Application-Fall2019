c4e9aa3def15e1b7edef158c51fe93b4d3c2e24d
skylert7 <skylert@smu.edu>
2019-10-30 22:53:02 -0500

    Sprint 1 done - USE THIS COMMIT for reference

--- election-buddy/src/App.js
+++ election-buddy/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import  { Redirect } from 'react-router-dom';
 import Nav from './Components/NavBar/Nav'
 import Footer from './Components/Footer/Footer';
 import Homepage from './Components/Homepage/Homepage';
 import Login from './Components/Login/Login';
 import Signup from './Components/Signup/Signup';
 
-var loggedin = true;
-var signedup = true;
+var loggedIn = true;
+var signedUp = true;
 
 function App() {
   return (
@@ -16,13 +17,18 @@ function App() {
     <Router>
       <Nav/>
         <Switch>
-          <Route exact path="/" component={Homepage}/>
+          <Route exact path="/" render={() => (
+            loggedIn ? (
+              <Redirect to="/login"/>
+            ) : (
+              <Homepage/>
+            )
+          )}/>
           <Route path="/login" component={Login}/>
           <Route path="/registration" component={Signup}/>
         </Switch>
       <Footer/>
     </Router>
-
     </div>
   );
 }
--- election-buddy/src/Components/Homepage/Homepage.js
+++ election-buddy/src/Components/Homepage/Homepage.js
@@ -3,14 +3,19 @@ import './Homepage.css';
 import Content from './Content.js';
 import Candidate from '../CandidateCards/Candidate.js';
 
+class Homepage extends Component {
+    constructor(props) {
+      super(props);
+    }
 
-function Homepage () {
-  return (
-    <div>
-      <Candidate/>
-      <Content/>
-    </div>
-  );
+    render() {
+      return (
+        <div>
+        <Candidate/>
+        <Content/>
+        </div>
+      );
+    }
 }
 
 export default Homepage;
