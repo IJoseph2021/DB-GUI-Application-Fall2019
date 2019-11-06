5e89f4ca849e51f182dfc6069aeb581de7ab3da7
pwsmith629 <parkers@smu.edu>
2019-11-06 11:48:03 -0600

    update and soft remove added to questions

--- election-buddy/database_server/src/questions.js
+++ election-buddy/database_server/src/questions.js
@@ -6,8 +6,6 @@ exports.setConnection = function(newMysqlConnection){
 
 //Creates a question
 
-
-
 exports.createQuestion = function(req,res){
 
     question_ID = req.params.question_ID;
@@ -15,9 +13,11 @@ exports.createQuestion = function(req,res){
     asker_ID = req.params.asker_ID;
     askee_ID = req.params.askee_ID;
     question = req.params.question;
+    active = 1;
+    update_Time = req.params.question_Time
 
-    query = "INSERT INTO electionBuddy.CANDIDATE_QUESTION (question_ID, question_Time, asker_ID, askee_ID, question)"+
-    " VALUES(\""+ question_ID + "\",\"" + question_Time + "\",\"" + asker_ID + "\",\"" + askee_ID + "\",\"" + question + "\");";
+    query = "INSERT INTO electionBuddy.CANDIDATE_QUESTION (question_ID, question_Time, asker_ID, askee_ID, question, active, update_Time)"+
+    " VALUES(\""+ question_ID + "\",\"" + question_Time + "\",\"" + asker_ID + "\",\"" + askee_ID + "\",\"" + question + "\",\"" + active + "\",\"" + update_Time + "\" );";
 
     console.log(query);
     mysqlConnection.query(query, 
@@ -46,3 +46,37 @@ exports.getQuestion = function(req, res){
         }
     });
 }
+
+//soft remove
+exports.removeQuestion = function(req, res){
+    qID = req.params.question_ID
+    console.log(`UPDATE CANDIDATE_QUESTION SET active = 0 WHERE question_ID = '${qID}';`);
+    mysqlConnection.query(`UPDATE CANDIDATE_QUESTION SET active = 0 WHERE question_ID = '${qID}';`,function(err,rows,fields){
+    
+        if(err){
+            res.send("Question Remove Failed");
+            }
+        else {
+            res.send("Changed to inactive... soft remove");
+        }        
+    });
+}
+
+exports.updateQuestion = function(req, res){
+    qID = req.params.question_ID
+    question2 = req.params.question2
+    update_Time = req.params.update_Time
+
+    console.log(`UPDATE CANDIDATE_QUESTION SET question = '${question2}', update_Time = '${update_Time}' WHERE question_ID = '${qID}';`);
+    mysqlConnection.query(`UPDATE CANDIDATE_QUESTION SET question = '${question2}', update_Time = '${update_Time}' WHERE question_ID = '${qID}';`,function(err,rows,fields){
+        if(err){
+            res.send("Error Updating Question");
+        }
+        else{
+            res.send("Question updated with new time stamp");
+        }
+    });
+}
+
+
+//remove, reply, soft delete, time stamps for create and update
