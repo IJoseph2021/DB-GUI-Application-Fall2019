4e8356b87756495ab0725e587eb9a6d1a27d5cc3
sshoemaker <sshoemaker@smu.edu>
2019-12-04 16:11:13 -0600

    fixed verify

--- New Config/backend/admin.js
+++ New Config/backend/admin.js
@@ -13,7 +13,7 @@ exports.getAdminLevel = function(req,res){
 
 //Verifies the candidate
 exports.verifyCandidate = function(req,res){
-    mysqlconnection.query(`UPDATE CANDIDATE SET verified = 1 WHERE userID = ${req.params.ID};`,function(err,row,fields){
+    mysqlconnection.query(`UPDATE CANDIDATE SET verified = 0 WHERE userID = ${req.params.ID};`,function(err,row,fields){
         if(!err){
             res.send("updated");
         } else {
